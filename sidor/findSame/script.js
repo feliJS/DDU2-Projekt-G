f6f3f1a24fd81ece967afe0625ce4493a86a9e38
Felicia <190894036+feliJS@users.noreply.github.com>
2024-12-26 21:08:00 +0100

    [Find] updateText() func, updates text content wth copies and clicked num
    updates text content with how many .marked classes there are and also the text of the box that you clicked on, also added the "reset" button that will change the text to the original text and also remove any box with the "marked" class

--- sidor/findSame/script.js
+++ sidor/findSame/script.js
@@ -1,7 +1,8 @@
 function handleBox(numBoxDOM) {
     numBoxDOM.addEventListener("click", () => {
-        numBoxDOM.classList.add("found")
+        numBoxDOM.classList.add("marked")
         markNum(numBoxDOM.textContent) //from common.js
+        updateText()
     })
 }
 
@@ -9,3 +10,16 @@ function handleCreation() {
 
 }
 
+function updateText() {
+    const foundBoxes = document.querySelectorAll(".marked")
+    foundText.textContent = `${foundBoxes.length} copies of the number ${foundBoxes[0].textContent}`
+}
+const foundText = document.querySelector("#most-repeated p")
+const resetBtn = document.querySelector("#most-repeated button")
+resetBtn.addEventListener("click", () => {
+    foundText.textContent = "Click on a number to find copies"
+    const boxes = document.querySelectorAll(".box")
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].classList.remove("marked")
+    }
+})
