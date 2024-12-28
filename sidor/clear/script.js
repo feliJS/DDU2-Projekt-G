df0bdb72b5666b1b518b92963e0aede9a1de5e02
Felicia <190894036+feliJS@users.noreply.github.com>
2024-12-28 17:06:28 +0100

    [Clear] fillClearedBtn that will remove any target class
    Added an eventListener to the fill cleared button that will loop thru all the boxes and check if any box has a "target" class in its classList, if it does, remove the target class and set the textContent to a random number

--- sidor/clear/script.js
+++ sidor/clear/script.js
@@ -12,3 +12,15 @@ function handleBox(numBoxDOM) { //also here, target class gets thrown around alo
 }
 
 function handleCreation() {/*nothing*/ }
+
+//could argue that the function bellow is already used in findSame? anyway i could fix so that they are different?
+const fillClearedBtn = document.querySelector("#fill-clear button")
+fillClearedBtn.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box")
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i].classList.contains("target")) {
+            boxes[i].classList.remove("target")
+            boxes[i].textContent = randomNum(0, 100)
+        }
+    }
+})
